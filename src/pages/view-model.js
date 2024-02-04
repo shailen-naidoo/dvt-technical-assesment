fe700c2a5dcbb7d4a8296b5da7643c61c2299b3b
Shailen Naidoo <naidooshailen648@gmail.com>
2024-02-04 15:49:19 +0200

    dvt-1
    src/pages/view-model.spec.js
    
    * Wrote happy case unit test to check if the list of products are being sent on the correct property on the Vue instance.
    
    src/pages/view-model.js
    
    * Created the logic so that the above test can pass.
    * I am setting the response from the API call to the reactive property on the Vue instance so that the view can use it to display it to the user.

--- src/pages/view-model.js
+++ src/pages/view-model.js
@@ -1,8 +1,14 @@
 import { ProductService } from "@/services/ProductService"
 
 const IndexViewModel = {
+  data() {
+    return {
+      products: [],
+    }
+  },
   mounted() {
     return ProductService.fetchListOfProducts()
+      .then((products) => this.products = products)
       .catch((err) => window.alert(err))
   }
 }
--- src/pages/view-model.spec.js
+++ src/pages/view-model.spec.js
@@ -1,9 +1,13 @@
 import { mount, flushPromises } from "@vue/test-utils";
-import { describe, test, expect, vi } from "vitest";
+import { describe, test, expect, vi, beforeEach } from "vitest";
 import Index from './Index.vue'
 import axios from "axios";
 
 vi.mock('axios')
+
+beforeEach(() => {
+  vi.resetAllMocks()
+})
 
 describe('Test Index.vue', () => {
   describe('Test the listing of the products and ensure that they get displayed to the user', () => {
@@ -22,6 +26,31 @@ describe('Test Index.vue', () => {
           expect(axios.get).toBeCalledWith('https://fakestoreapi.com/products')
           expect(alertSpy).toBeCalledTimes(1)
           expect(alertSpy).toBeCalledWith('Network request failed!')
+        })
+      })
+    })
+
+    describe('Happy cases', () => {
+      test('When the user visits the webapp it should fetch a list of products and display it to the user', () => {
+        // 1. SETUP
+        axios.get.mockResolvedValueOnce({
+          data: [
+            { title: 'A', price: 1000, category: 'Electronics', description: 'Hello World', image: 'https://image.com' },
+            { title: 'B', price: 500, category: 'Electronics', description: 'Hello World', image: 'https://image.com' },
+          ]
+        })
+
+        // 2. ACTION
+        const wrapper = mount(Index)
+
+        return flushPromises().then(() => {
+          // 3. ASSERT
+          expect(axios.get).toBeCalledTimes(1)
+          expect(axios.get).toBeCalledWith('https://fakestoreapi.com/products')
+          expect(wrapper.vm.products).toEqual([
+            { title: 'A', price: 1000, category: 'Electronics', description: 'Hello World', image: 'https://image.com' },
+            { title: 'B', price: 500, category: 'Electronics', description: 'Hello World', image: 'https://image.com' },
+          ])
         })
       })
     })
