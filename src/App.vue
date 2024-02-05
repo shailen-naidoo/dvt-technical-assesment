465c58d41c1f89f900382c14507ce1e9c92bff88
Shailen Naidoo <naidooshailen648@gmail.com>
2024-02-05 10:38:26 +0200

    dvt-2
    src/App.vue
    
    * Created a simple navbar just so that we have some kind of navigation system for the user to use to get to the cart https://docs.google.com/document/d/1HUKDlBX8jGUUyK4EXUe1YZ3XIbezKgEf6fWmlBgYJ0c/edit#bookmark=id.bjp7ybqfdygw

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
 <template>
+  <nav class="bg-gray-800">
+    <section class="container mx-auto px-4 py-2 flex justify-between items-center">
+      <a href="/" class="font-bold text-white text-2xl">DVT Vue Storefront</a>
+      <ul class="hidden md:flex space-x-4 text-white">
+        <li><a href="#" class="hover:text-gray-400">View Cart</a></li>
+      </ul>
+    </section>
+  </nav>
   <RouterView />
 </template>
