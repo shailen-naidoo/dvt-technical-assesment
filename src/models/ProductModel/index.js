9ba60ab2a88467435ebfbce81fb7ad26da9d62a9
Shailen Naidoo <naidooshailen648@gmail.com>
2024-02-06 06:48:08 +0200

    src/models/ProductModel/index.js
    * Added JSDoc to the ProductModel to provide some typing to the param

--- src/models/ProductModel/index.js
+++ src/models/ProductModel/index.js
@@ -1,15 +1,32 @@
 import { BASE_QUANTITY } from "./constants"
 
+/**
+ * Represents a product model.
+ */
 class ProductModel {
+  /**
+   * Creates a new instance of ProductModel.
+   *
+   * @param {object} product An object containing product data.
+   * @param {string} product.title The title of the product.
+   * @param {number} product.price The price of the product.
+   * @param {string} product.category The category of the product.
+   * @param {string} product.description The description of the product.
+   * @param {string} product.image The image URL of the product.
+   */
   constructor(product) {
-    this.title = product.title
-    this.price = product.price
-    this.category = product.category
-    this.description = product.description
-    this.image = product.image
+    this.title = product.title;
+    this.price = product.price;
+    this.category = product.category;
+    this.description = product.description;
+    this.image = product.image;
 
-    // values that are defined by us
-    this.quantity = BASE_QUANTITY
+    /**
+     * The quantity of the product.
+     *
+     * @type {number}
+     */
+    this.quantity = BASE_QUANTITY;
   }
 }
 
