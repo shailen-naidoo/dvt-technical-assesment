10e19a3f8b2125ff33e3df2d36f5602bf94d6705
Shailen Naidoo <naidooshailen648@gmail.com>
2024-02-06 06:30:08 +0200

    src/services/ProductService/index.js
    * Added JSDoc to the ProductService.fetchListOfProducts method so that call site functions get typing and intellisense

--- src/services/ProductService/index.js
+++ src/services/ProductService/index.js
@@ -2,6 +2,7 @@ import { ProductModel } from "@/models/ProductModel"
 import { HTTPService } from "../HTTPservice"
 
 class ProductService {
+  /** @returns {Promise<Array<ProductModel>>}*/
   static fetchListOfProducts = () => {
     return HTTPService.get('https://fakestoreapi.com/products')
       .then((res) => res.data.map((product) => new ProductModel(product)))
